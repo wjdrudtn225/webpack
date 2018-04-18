18f2ba9c8290ebe8f259bd13cdf50fb3ea1c85e8
wjdru <wjdrudtn0225@naver.com>
2018-04-18 14:02:07 +0900

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        filename: 'out/bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /.js$/,
+                loader: 'babel-loader',
+                options: {
+                    presets:['es2015', 'react']
+                }
+            }
+        ]
+    }
+};
